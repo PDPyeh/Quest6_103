fe4fab7a9946b74369f72286832fe9f8d16ca2de
Pradipa Y <pdpyyo@gmail.com>
2025-11-20 17:00:44 +0700

    nambah func dataapp di navigasi.kt

--- app/src/main/java/com/example/viewmodel/navigasi.kt
+++ app/src/main/java/com/example/viewmodel/navigasi.kt
@@ -1,3 +1,52 @@
 package com.example.viewmodel
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.viewmodel.view.FormSiswa
+import com.example.viewmodel.view.TampilSiswa
+import com.example.viewmodel.viewmodel.SiswaViewModel
+
+enum class Navigasi {
+    Formulirku,
+    Detail
+}
+
 @Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier,
+    viewModel: SiswaViewModel = viewModel()
+){
+    Scaffold { isiRuang->
+        val uiState = viewModel.statusUI.collectAsState()
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulirku.name,
+
+            modifier = Modifier.padding(paddingValues = isiRuang)){
+            composable(route = Navigasi.Formulirku.name){
+                FormSiswa (
+                    OnSubmitButtonClicked = {
+                        navController.navigate(route = Navigasi.Detail.name)
+                    }
+                )
+            }
+            composable(route = Navigasi.Detail.name) {
+                TampilSiswa(
+                    onBackButtonClicked = {
+                        cancelAndBackToFormulirku(navController)
+                    }
+                )
+            }
+
+        }
+    }
+}
