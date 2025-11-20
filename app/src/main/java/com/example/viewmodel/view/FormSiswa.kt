be40c3f78b89ecc2c8f37660246bd32ea2f9edbc
Pradipa Y <pdpyyo@gmail.com>
2025-11-20 16:00:16 +0700

    menambahkan column di Scaffold di function formSiswa

--- app/src/main/java/com/example/viewmodel/view/FormSiswa.kt
+++ app/src/main/java/com/example/viewmodel/view/FormSiswa.kt
@@ -1,14 +1,27 @@
 package com.example.viewmodel.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import com.example.viewmodel.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormSiswa(
     pilihanJK: List<String>,
@@ -21,8 +34,26 @@ fun FormSiswa(
     val listData: MutableList<String> = mutableListOf(txtNama,txtGender,txtAlamat)
 
     Scaffold(modifier = Modifier,
-        {TopAppBar(tittle)}
-        )
+        {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.app_name),
+                        color = Color.White
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors
+                    (containerColor = colorResource(id = R.color.teal_700))
+            )
+        }
+    ){isiRuang ->
+        Column(modifier = Modifier.padding(paddingValues = isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
 
+        }
+
+    }
 
 }
